65a3f5bb6a64cbc4d0728cdf911b651b537992a0
Aiden <aiden.gimpel@ontariotechu.net>
2021-10-09 14:24:23 -0400

    Made it so 2 obstacles spawn at each spawn point

--- Assets/[Scripts]/ObstacleSpawner.cs
+++ Assets/[Scripts]/ObstacleSpawner.cs
@@ -21,8 +21,20 @@ public class ObstacleSpawner : MonoBehaviour
     void SpawnObstacle()
     {
         int obstacleSpawnIndex = Random.Range(0, 3);
+        int obstacleSpawnIndex2 = Random.Range(0, 3);
+
+        if (obstacleSpawnIndex == obstacleSpawnIndex2)
+        {
+            while(obstacleSpawnIndex2 == obstacleSpawnIndex)
+            {
+                obstacleSpawnIndex2 = Random.Range(0, 3);
+            }
+        }
+
         Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
+        Transform spawnPoint2 = transform.GetChild(obstacleSpawnIndex2).transform;
 
         Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
+        Instantiate(obstaclePrefab, spawnPoint2.position, Quaternion.identity, transform);
     }
 }
--- Assets/[Scripts]/Player.cs
+++ Assets/[Scripts]/Player.cs
@@ -47,7 +47,6 @@ public class Player : MonoBehaviour
         Lose();
 
         Move();
-    
     }
 
     void Move()
