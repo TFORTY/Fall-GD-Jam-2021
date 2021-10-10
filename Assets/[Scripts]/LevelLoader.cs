e1c21b7003d8e84640f8b1b3ed88a3dccbb4f4b0
Tyler <tyler.wong@ontariotechu.net>
2021-10-09 23:53:00 -0400

    Added progressive level switch

---
+++ Assets/[Scripts]/LevelLoader.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int ilevelToLoad;
+    [SerializeField] string slevelToLoad;
+    [SerializeField] bool useIntergerToLoadLevel = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject collisionGameObject = collision.gameObject;
+
+        if (collisionGameObject.tag == "Player")
+        {
+            LoadScene();
+        }
+    }
+
+    void LoadScene()
+    {
+        if (useIntergerToLoadLevel)
+        {
+            SceneManager.LoadScene(ilevelToLoad);
+
+        }
+        else
+        {
+            SceneManager.LoadScene(slevelToLoad);
+        }
+    }
+}
